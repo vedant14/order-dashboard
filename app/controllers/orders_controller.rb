0a23baac6b5c23fc2effcd73ca292122cb1c3df6
Vedant Lohbare <vedant.lohbare@gmail.com>
2020-06-06 20:47:44 +0530

    Ajax add and delete functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,55 @@
 class OrdersController < ApplicationController
-  def new
-  end
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   def index
   	@orders = Order.filter(params.slice(:status))
   end
 
-  def create
+  def show
   end
 
-  def show
-	@order = Order.find(params[:id])
+  def new
+    @order = Order.new
   end
+
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      respond_to do |format| 
+        format.js
+        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+      end                    
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
   def update
+      respond_to do |format|
+        format.html { redirect_to root_path}
+        format.js
+      end
   end
+
+  def destroy
+    @order.destroy
+      respond_to do |format| 
+        format.js
+        format.html { redirect_to root_path, notice: 'Order was successfully deleted.' }
+      end                    
+  end
+
+private
+  def set_order
+   @order = Order.find(params[:id])
+  end 
+
+  def order_params
+    params.require(:order).permit(:name)
+  end
+
 end
